f14e1d7ced63795cb767bdd76bc7e8c4026bf820
Lakshya <lakshyasingh04im@gmail.com>
2026-05-28 07:19:39 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// function joinGym(){
+
+// alert("Welcome To Muscle Fit Gym!");
+
+// }
+function joinGym(){
+
+window.open(
+"https://wa.me/9140661828?text=Hello%20I%20want%20to%20join%20The%20Fit%20Muscle%20Gym",
+"_blank"
+);
+
+}
+function contactOwner(){
+
+window.open(
+"https://wa.me/919876543210?text=Hello%20I%20want%20information%20about%20Muscle%20Fit%20Gym",
+"_blank"
+);
+
+}
+function ownerPhone(){
+
+window.location.href = "tel:+91 9140661828";
+
+}
